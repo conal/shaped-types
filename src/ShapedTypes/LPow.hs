3cd0a125b730ee64d3fa0c4cbd1ef1cc1ef9d1a0
Conal Elliott <conal@conal.net>
2016-03-29 19:20:29 -0700

    "Pow" --> "LPow" for easier core reading

--- src/ShapedTypes/LPow.hs
+++ src/ShapedTypes/LPow.hs
@@ -37,7 +37,7 @@ AbsTyPragmas
 -- Right-associated functor exponentiation
 ----------------------------------------------------------------------
 
-#define UseGenerics
+-- #define UseGenerics
 
 module ShapedTypes.LPow where
 
@@ -80,52 +80,54 @@ import ShapedTypes.Scan
 infix 8 ^  -- infixr is ill-kinded, while infixl is contrary to convention
 
 -- Top-down, depth-typed, perfect, binary, leaf trees
-data Pow :: (* -> *) -> Nat -> * -> * where
-  L :: a -> Pow h Z a
-  B :: Pow h n (h a) -> Pow h (S n) a
+data LPow :: (* -> *) -> Nat -> * -> * where
+  L :: a -> LPow h Z a
+  B :: LPow h n (h a) -> LPow h (S n) a
 
-type (^) = Pow
+-- I use "LPow" instead of "Pow" to make compiler output easier to follow.
 
-unL :: Pow h Z a -> a
+type (^) = LPow
+
+unL :: LPow h Z a -> a
 unL (L a) = a
 
-unB :: Pow h (S n) a -> Pow h n (h a)
+unB :: LPow h (S n) a -> LPow h n (h a)
 unB (B p) = p
 
-inL :: (a -> b) -> (Pow h Z a -> Pow h Z b)
+inL :: (a -> b) -> (LPow h Z a -> LPow h Z b)
 inL = L <~ unL
 
-inB :: (Pow h m (h a) -> Pow h n (h b))
-    -> (Pow h (S m) a -> Pow h (S n) b)
+inB :: (LPow h m (h a) -> LPow h n (h b))
+    -> (LPow h (S m) a -> LPow h (S n) b)
 inB = B <~ unB
 
-inL2 :: (a -> b -> c) -> (Pow h Z a -> Pow h Z b -> Pow h Z c)
+inL2 :: (a -> b -> c) -> (LPow h Z a -> LPow h Z b -> LPow h Z c)
 inL2 = inL <~ unL
 
-inB2 :: (Pow h m (h a) -> Pow h n (h b) -> Pow h o (h c))
-     -> (Pow h (S m) a -> Pow h (S n) b -> Pow h (S o) c)
+inB2 :: (LPow h m (h a) -> LPow h n (h b) -> LPow h o (h c))
+     -> (LPow h (S m) a -> LPow h (S n) b -> LPow h (S o) c)
 inB2 = inB <~ unB
 
 {--------------------------------------------------------------------
     Standard type class instances
 --------------------------------------------------------------------}
 
-instance Functor (Pow h Z) where
+instance Functor (LPow h Z) where
   fmap f (L a ) = L (f a)
   {-# INLINABLE fmap #-}
 
-instance (Functor h, Functor (Pow h n)) => Functor (Pow h (S n)) where
+instance (Functor h, Functor (LPow h n)) => Functor (LPow h (S n)) where
   fmap f (B ts) = B ((fmap.fmap) f ts)
   {-# INLINABLE fmap #-}
   SPECS(Functor)
 
-instance Applicative (Pow h Z) where
+instance Applicative (LPow h Z) where
   pure a = L a
   L f <*> L a = L (f a)
   {-# INLINABLE pure #-}
   {-# INLINABLE (<*>) #-}
 
-instance (Applicative h, Applicative (Pow h n)) => Applicative (Pow h (S n)) where
+instance (Applicative h, Applicative (LPow h n)) => Applicative (LPow h (S n)) where
   pure a = B (pure (pure a))
   B fs <*> B xs = B (liftA2 (<*>) fs xs)
   {-# INLINABLE pure #-}
@@ -134,108 +136,108 @@ instance (Applicative h, Applicative (Pow h n)) => Applicative (Pow h (S n)) whe
 
 -- TODO: Monad
 
-instance Foldable (Pow h Z) where
+instance Foldable (LPow h Z) where
   foldMap f (L a) = f a
   {-# INLINABLE foldMap #-}
 
-instance (Foldable h, Foldable (Pow h n)) => Foldable (Pow h (S n)) where
+instance (Foldable h, Foldable (LPow h n)) => Foldable (LPow h (S n)) where
   foldMap f (B ts) = (foldMap.foldMap) f ts
   {-# INLINABLE foldMap #-}
   SPECS(Foldable)
 
-instance Traversable (Pow h Z) where
+instance Traversable (LPow h Z) where
   traverse f (L a ) = L <$> f a
   {-# INLINABLE traverse #-}
 
-instance (Traversable h, Traversable (Pow h n)) => Traversable (Pow h (S n)) where
+instance (Traversable h, Traversable (LPow h n)) => Traversable (LPow h (S n)) where
   traverse f (B ts) = B <$> (traverse.traverse) f ts
   {-# INLINABLE traverse #-}
   SPECS(Traversable)
 
-instance Eq a => Eq (Pow h Z a) where
+instance Eq a => Eq (LPow h Z a) where
   (==) = (==) `on` unL
 
-instance Eq (Pow h n (h a)) => Eq (Pow h (S n) a) where
+instance Eq (LPow h n (h a)) => Eq (LPow h (S n) a) where
   (==) = (==) `on` unB
 
-instance Ord a => Ord (Pow h Z a) where
+instance Ord a => Ord (LPow h Z a) where
   compare = compare `on` unL
 
-instance Ord (Pow h n (h a)) => Ord (Pow h (S n) a) where
+instance Ord (LPow h n (h a)) => Ord (LPow h (S n) a) where
   compare = compare `on` unB
 
-instance Show a => Show (Pow h Z a) where
+instance Show a => Show (LPow h Z a) where
   showsPrec p (L a)  = showsUnary "L" p a
 
-instance Show (Pow h n (h a)) => Show (Pow h (S n) a) where
+instance Show (LPow h n (h a)) => Show (LPow h (S n) a) where
   showsPrec p (B ts)  = showsUnary "B" p ts
 
-instance Arbitrary a => Arbitrary (Pow h Z a) where
+instance Arbitrary a => Arbitrary (LPow h Z a) where
   arbitrary    = L <$> arbitrary
   shrink (L a) = L <$> shrink a
 
-instance Arbitrary (Pow h n (h a)) => Arbitrary (Pow h (S n) a) where
+instance Arbitrary (LPow h n (h a)) => Arbitrary (LPow h (S n) a) where
   arbitrary    = B <$> arbitrary
   shrink (B a) = B <$> shrink a
 
-instance CoArbitrary a => CoArbitrary (Pow h Z a) where
+instance CoArbitrary a => CoArbitrary (LPow h Z a) where
   coarbitrary (L a) = coarbitrary a
 
-instance CoArbitrary (Pow h n (h a)) => CoArbitrary (Pow h (S n) a) where
+instance CoArbitrary (LPow h n (h a)) => CoArbitrary (LPow h (S n) a) where
   coarbitrary (B a) = coarbitrary a
 
 {--------------------------------------------------------------------
     keys package
 --------------------------------------------------------------------}
 
-type instance Key (Pow h m) = Vec m (Key h)
+type instance Key (LPow h m) = Vec m (Key h)
 
 -- TODO: Consider using snoc vectors
 
-instance Keyed (Pow h Z) where
+instance Keyed (LPow h Z) where
   mapWithKey q = inL (q ZVec)
 
-instance (Keyed h, Keyed (Pow h n)) => Keyed (Pow h (S n)) where
+instance (Keyed h, Keyed (LPow h n)) => Keyed (LPow h (S n)) where
   mapWithKey q = inB (mapWithKey (mapWithKey . fmap q . flip (:<)))
 
 -- See RPow source for typings
 
-instance (Functor (Pow n h), Applicative (Pow n h)) => Zip (Pow n h) where
+instance (Functor (LPow n h), Applicative (LPow n h)) => Zip (LPow n h) where
   zipWith = liftA2
 
 -- Without the seemingly redundant Functor (Vec n) constraint, GHC 8.1.20160307 says
 -- 
---     • Could not deduce (Functor (Pow n h))
+--     • Could not deduce (Functor (LPow n h))
 --         arising from the superclasses of an instance declaration
---       from the context: Applicative (Pow n h)
+--       from the context: Applicative (LPow n h)
 --         bound by the instance declaration
 --         at src/ShapedTypes/LPow.hs:202:10-47
 --
 -- Perhaps <https://ghc.haskell.org/trac/ghc/ticket/11427>.
 
-instance (Applicative (Pow n h), Keyed (Pow n h)) => ZipWithKey (Pow n h)
+instance (Applicative (LPow n h), Keyed (LPow n h)) => ZipWithKey (LPow n h)
 
-instance Indexable (Pow h n) => Lookup (Pow h n) where
+instance Indexable (LPow h n) => Lookup (LPow h n) where
   lookup k t = Just (index t k)
 
-instance Indexable (Pow h Z) where
+instance Indexable (LPow h Z) where
   index (L a) ZVec = a
 
-instance (Indexable h, Indexable (Pow h n)) => Indexable (Pow h (S n)) where
+instance (Indexable h, Indexable (LPow h n)) => Indexable (LPow h (S n)) where
   index (B ts) (k :< ks) = ts ! ks ! k
 
-instance Adjustable (Pow h Z) where
+instance Adjustable (LPow h Z) where
   adjust f ZVec = inL f
 
-instance (Adjustable h, Adjustable (Pow h n)) => Adjustable (Pow h (S n)) where
+instance (Adjustable h, Adjustable (LPow h n)) => Adjustable (LPow h (S n)) where
   adjust f (k :< ks) = inB (adjust (adjust f k) ks)
 
 {- -- Worth implementing?
 
-instance (Foldable (Pow n h), Keyed (Pow n h)) => FoldableWithKey (Pow n h) where
+instance (Foldable (LPow n h), Keyed (LPow n h)) => FoldableWithKey (LPow n h) where
   foldMapWithKey f = foldMap (uncurry f) . keyed
 
-instance (Traversable (Pow n h), Keyed (Pow n h)) => TraversableWithKey (Pow n h) where
+instance (Traversable (LPow n h), Keyed (LPow n h)) => TraversableWithKey (LPow n h) where
   traverseWithKey f = traverse (uncurry f) . keyed
 -}
 
@@ -243,23 +245,23 @@ instance (Traversable (Pow n h), Keyed (Pow n h)) => TraversableWithKey (Pow n h
     Other representations
 --------------------------------------------------------------------}
 
-instance Generic1 (Pow h Z) where
-  type Rep1 (Pow h Z) = Par1
+instance Generic1 (LPow h Z) where
+  type Rep1 (LPow h Z) = Par1
   from1 = Par1 . unL
   to1   = L . unPar1
 
-instance Generic1 (Pow h (S n)) where
-  type Rep1 (Pow h (S n)) = Pow h n :.: h
+instance Generic1 (LPow h (S n)) where
+  type Rep1 (LPow h (S n)) = LPow h n :.: h
   from1 = Comp1 . unB
   to1   = B . unComp1
 
-instance HasRep (Pow h Z a) where
-  type Rep (Pow h Z a) = a
+instance HasRep (LPow h Z a) where
+  type Rep (LPow h Z a) = a
   repr (L a) = a
   abst = L
 
-instance HasRep (Pow h (S n) a) where
-  type Rep (Pow h (S n) a) = Pow h n (h a)
+instance HasRep (LPow h (S n) a) where
+  type Rep (LPow h (S n) a) = LPow h n (h a)
   repr (B t) = t
   abst t = B t
 
@@ -267,31 +269,31 @@ instance HasRep (Pow h (S n) a) where
     shaped-types instances
 --------------------------------------------------------------------}
 
-instance (Foldable (Pow h n), ApproxEq a) => ApproxEq (Pow h n a) where
+instance (Foldable (LPow h n), ApproxEq a) => ApproxEq (LPow h n a) where
   (=~) = approxEqFoldable
 
 -- -- Compute size @h exactly once where genericSize would compute it n times.
 -- instance (Sized h, Foldable (Vec n), Applicative (Vec n))
---       => Sized (Pow h n) where
+--       => Sized (LPow h n) where
 --   size = product (pure (size @h)  :: Vec n Int)
 
-instance (Sized h, Sized (Vec n)) => Sized (Pow h n) where
+instance (Sized h, Sized (Vec n)) => Sized (LPow h n) where
   size = size @h ^ size @(Vec n)
   {-# INLINE size #-}
 
 #ifdef UseGenerics
 
-instance (Generic1 (Pow h n), LScan (Rep1 (Pow h n))) => LScan (Pow h n) where
+instance (Generic1 (LPow h n), LScan (Rep1 (LPow h n))) => LScan (LPow h n) where
   lscan = genericLscan
   {-# INLINE lscan #-}
 
 #else
 
-instance LScan (Pow h Z) where
+instance LScan (LPow h Z) where
   lscan (L a) = (L mempty, a)
   {-# INLINE lscan #-}
-instance (LFScan h, LScan (Pow h n), Applicative (Pow h n))
-      => LScan (Pow h (S n)) where
+instance (LFScan h, LScan (LPow h n), Applicative (LPow h n))
+      => LScan (LPow h (S n)) where
   lscan (B ts) = first B (lscanComp ts)
   {-# INLINE lscan #-}
 
@@ -301,5 +303,5 @@ instance (LFScan h, LScan (Pow h n), Applicative (Pow h n))
     Circuit support
 --------------------------------------------------------------------}
 
-AbsTy(Pow h   Z   a)
-AbsTy(Pow h (S n) a)
+AbsTy(LPow h   Z   a)
+AbsTy(LPow h (S n) a)
