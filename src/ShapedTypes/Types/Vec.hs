cc9a1c1ac7c5a5b873ee57d6b007b2f960625cd7
Conal Elliott <conal@conal.net>
2016-04-04 19:41:21 -0700

    move circuit support to Types.Vec

--- src/ShapedTypes/Types/Vec.hs
+++ src/ShapedTypes/Types/Vec.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE CPP                 #-}
 {-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE GADTs               #-}
+{-# LANGUAGE InstanceSigs        #-}
 {-# LANGUAGE KindSignatures      #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeOperators       #-}
 
 #include "Circat/AbsTy.inc"
 
@@ -29,7 +32,14 @@ module ShapedTypes.Types.Vec (Vec(..)) where
 
 import Prelude hiding (id,(.))
 
+import Circat.Category (exl,exr,id,second,twiceP,(&&&),(***),(.),Uncurriable(..))
+import Circat.Circuit
+import Circat.Classes (unitIf)
+import Circat.Classes (BottomCat (..),IfCat (..))
+import Circat.Misc ((:*),Unit)
 import Circat.Rep
+
+#include "Circat/AbsTy.inc"
 
 import ShapedTypes.Nat
 
@@ -51,3 +61,104 @@ instance HasRep (Vec (S n) a) where
   type Rep (Vec (S n) a) = (a,Vec n a)
   repr (a :< as) = (a, as)
   abst (a, as) = (a :< as)
+
+{--------------------------------------------------------------------
+    Circuit support
+--------------------------------------------------------------------}
+
+#if 0
+AbsTy(Vec Z a)
+AbsTy(Vec (S n) a)
+
+-- TODO: custom AbsTy replacement for Vec, as I'll be using it for n-ary trees.
+#else
+
+instance GenBuses q_q => Uncurriable (:>) q_q (Vec n a) where
+  uncurries = id
+
+instance (Applicative (Vec n), Traversable (Vec n), GenBuses a) => GenBuses (Vec n a) where
+  genBuses' prim ins = buses <$> sequenceA (pure gb)
+   where
+     gb :: BusesM (Buses a)
+     gb = genBuses' prim ins
+     {-# NOINLINE gb #-}  -- still necessary?
+     buses :: Vec m (Buses a) -> Buses (Vec m a)
+     buses ZVec = abstB UnitB
+     buses (b :< bs) = abstB (PairB b (buses bs))
+
+  delay :: Vec n a -> (Vec n a :> Vec n a)
+  delay = go
+   where
+     go :: Vec m a -> (Vec m a :> Vec m a)
+     go ZVec = id
+     go (a :< as) = abstC . (del a *** go as) . reprC
+     del :: a -> (a :> a)
+     del = delay
+     {-# NOINLINE del #-}  -- still necessary?
+
+  ty :: Vec n a -> Ty
+  ty = const (foldr PairT UnitT (pure t :: Vec n Ty))
+   where
+     t = ty (undefined :: a)
+     {-# NOINLINE t #-}
+
+instance (Applicative (Vec n), BottomCat (:>) a) => BottomCat (:>) (Vec n a) where
+  bottomC :: Unit :> Vec n a
+  bottomC = go (pure ())
+   where
+     go :: Vec m () -> (Unit :> Vec m a)
+     go ZVec = abstC
+     go (() :< units) = abstC . (bc &&& go units)
+     bc :: Unit :> a
+     bc = bottomC
+     {-# NOINLINE bc #-}
+
+instance (Applicative (Vec n), IfCat (:>) a) => IfCat (:>) (Vec n a)
+ where
+   ifC :: Bool :* (Vec n a :* Vec n a) :> Vec n a
+   ifC = go (pure ())
+    where
+      go :: Vec m () -> Bool :* (Vec m a :* Vec m a) :> Vec m a
+      go ZVec = abstC . unitIf . second (twiceP reprC)
+      go (() :< units) = abstC
+                       . ((ifc . second (twiceP exl)) &&& (go units . second (twiceP exr)))
+                       . second (twiceP reprC)
+      ifc :: Bool :* (a :* a) :> a
+      ifc = ifC
+      {-# NOINLINE ifc #-}
+
+-- Without these NOINLINE pragmas, GHC's typechecker does exponential work for
+-- n-ary trees.
+
+#if 0
+reprC :: Vec (S m) a :> Rep (Vec (S m) a)
+twiceP reprC :: Vec (S m) a :* Vec (S m) a :> Rep (Vec (S m) a) :* Rep (Vec (S m) a)
+second (twiceP reprC)
+  :: Bool :* (Vec (S m) a :* Vec (S m) a) :> Bool :* (Rep (Vec (S m) a) :* Rep (Vec (S m) a))
+  :: Bool :* (Vec (S m) a :* Vec (S m) a) :> Bool :* (a :* Vec m a) :* (a :* Vec m a)
+
+second (twiceP exl) :: Bool :* (a :* Vec m a) :* (a :* Vec m a) :> Bool :* a :* a
+ifc . second (twiceP exl) :: Bool :* (a :* Vec m a) :* (a :* Vec m a) :> a
+
+second (twiceP exr)
+  :: Bool :* (a :* Vec m a) :* (a :* Vec m a) :> Bool :* (Vec m a :* Vec m a)
+go units :: Bool :* (Vec m a :* Vec m a) :> Vec m a
+go units . second (twiceP exr) :: Bool :* (a :* Vec m a) :* (a :* Vec m a) :> Vec m a
+
+(ifc . second (twiceP exl)) &&& (go units . second (twiceP exr))
+  :: Bool :* (a :* Vec m a) :* (a :* Vec m a) :> a :* Vec m a
+
+abstC . ((ifc . second (twiceP exl)) &&& (go units . second (twiceP exr))) . second (twiceP reprC)
+  :: Bool :* (Vec (S m) a) :* (Vec (S m) a) :> Vec (S m) a
+#endif
+
+-- TODO: Look for simple formulations
+
+-- Without NOINLINE pragmas, GHC's typechecker does exponential work for
+-- trees.
+--
+-- TODO: Try again without NOINLINE, since I've reworked these definitions.
+
+-- TODO: Abstract these definitions into something reusable.
+
+#endif
--- src/ShapedTypes/Vec.hs
+++ src/ShapedTypes/Vec.hs
@@ -1,29 +1,26 @@
-{-# LANGUAGE CPP                 #-}
-{-# LANGUAGE ConstraintKinds     #-}
-{-# LANGUAGE DataKinds           #-}
-{-# LANGUAGE DeriveDataTypeable  #-}
-{-# LANGUAGE DeriveFoldable      #-}
-{-# LANGUAGE DeriveFunctor       #-}
-{-# LANGUAGE DeriveGeneric       #-}
-{-# LANGUAGE DeriveTraversable   #-}
-{-# LANGUAGE FlexibleContexts    #-}
-{-# LANGUAGE FlexibleInstances   #-}
-{-# LANGUAGE GADTs               #-}
-{-# LANGUAGE InstanceSigs        #-}
-{-# LANGUAGE KindSignatures      #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications    #-}
-{-# LANGUAGE TypeFamilies        #-}
-{-# LANGUAGE TypeOperators       #-}
-
-#include "Circat/AbsTy.inc"
-
-AbsTyPragmas
+{-# LANGUAGE CPP                  #-}
+{-# LANGUAGE ConstraintKinds      #-}
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE DeriveDataTypeable   #-}
+{-# LANGUAGE DeriveFoldable       #-}
+{-# LANGUAGE DeriveFunctor        #-}
+{-# LANGUAGE DeriveGeneric        #-}
+{-# LANGUAGE DeriveTraversable    #-}
+{-# LANGUAGE FlexibleContexts     #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE GADTs                #-}
+{-# LANGUAGE InstanceSigs         #-}
+{-# LANGUAGE KindSignatures       #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
+{-# LANGUAGE TypeApplications     #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 {-# OPTIONS_GHC -Wall #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
-{-# OPTIONS_GHC -fno-warn-unused-imports #-} -- TEMP
+-- {-# OPTIONS_GHC -fno-warn-unused-imports #-} -- TEMP
 -- {-# OPTIONS_GHC -fno-warn-unused-binds   #-} -- TEMP
 
 ----------------------------------------------------------------------
@@ -38,7 +35,7 @@ AbsTyPragmas
 -- Length-typed lists/vectors
 ----------------------------------------------------------------------
 
-{-# OPTIONS_GHC -fplugin=ReificationRules.Plugin -dcore-lint -fexpose-all-unfoldings #-}
+-- {-# OPTIONS_GHC -fplugin=ReificationRules.Plugin -dcore-lint -fexpose-all-unfoldings #-}
 
 -- {-# OPTIONS_GHC -fplugin-opt=ReificationRules.Plugin:trace #-}
 -- {-# OPTIONS_GHC -ddump-rule-rewrites #-}
@@ -46,26 +43,16 @@ AbsTyPragmas
 module ShapedTypes.Vec (Vec(..)) where
 
 import Prelude hiding (id,(.))
-import Circat.Category (id,(.),(***),(&&&),second,twiceP,exl,exr)
 import Control.Applicative (liftA2)
 import Data.Monoid ((<>))
 import GHC.Generics (Generic1(..),U1(..),Par1(..),(:*:)(..))
 
 import Data.Key
 
-import Circat.Classes (unitIf)
-import Circat.Misc (Unit,(:*))
-
-AbsTyImports
-import Circat.Circuit                   -- TODO: specific imports
-
 import ShapedTypes.ApproxEq
 import ShapedTypes.Sized
 import ShapedTypes.Nat
 import ShapedTypes.Scan (LScan(..),lscanTraversable)
-
--- Experiment
-import ShapedTypes.Pair
 
 import ShapedTypes.Types.Vec
 
@@ -143,6 +130,8 @@ instance Generic1 (Vec (S n)) where
 instance (Functor (Vec n), Applicative (Vec n)) => Zip (Vec n) where
   zipWith = liftA2
 
+-- Needs UndecidableInstances
+
 -- Without the seemingly redundant Functor (Vec n) constraint, GHC 8.1.20160307 says
 -- 
 --     • Could not deduce (Functor (Vec n))
@@ -204,103 +193,4 @@ instance Traversable (Vec n) => LScan (Vec n) where
   lscan = lscanTraversable
   {-# INLINE lscan #-}
 
-{--------------------------------------------------------------------
-    Circuit support
---------------------------------------------------------------------}
-
-#if 0
-AbsTy(Vec Z a)
-AbsTy(Vec (S n) a)
-
--- TODO: custom AbsTy replacement for Vec, as I'll be using it for n-ary trees.
-#else
-
-instance GenBuses q_q => Uncurriable (:>) q_q (Vec n a) where
-  uncurries = id
-
-instance (Applicative (Vec n), Traversable (Vec n), GenBuses a) => GenBuses (Vec n a) where
-  genBuses' prim ins = buses <$> sequenceA (pure gb)
-   where
-     gb :: BusesM (Buses a)
-     gb = genBuses' prim ins
-     {-# NOINLINE gb #-}  -- still necessary?
-     buses :: Vec m (Buses a) -> Buses (Vec m a)
-     buses ZVec = abstB UnitB
-     buses (b :< bs) = abstB (PairB b (buses bs))
-
-  delay :: Vec n a -> (Vec n a :> Vec n a)
-  delay = go
-   where
-     go :: Vec m a -> (Vec m a :> Vec m a)
-     go ZVec = id
-     go (a :< as) = abstC . (del a *** go as) . reprC
-     del :: a -> (a :> a)
-     del = delay
-     {-# NOINLINE del #-}  -- still necessary?
-
-  ty :: Vec n a -> Ty
-  ty = const (foldr PairT UnitT (pure t :: Vec n Ty))
-   where
-     t = ty (undefined :: a)
-     {-# NOINLINE t #-}
-
-instance (Applicative (Vec n), BottomCat (:>) a) => BottomCat (:>) (Vec n a) where
-  bottomC :: Unit :> Vec n a
-  bottomC = go (pure ())
-   where
-     go :: Vec m () -> (Unit :> Vec m a)
-     go ZVec = abstC
-     go (() :< units) = abstC . (bc &&& go units)
-     bc :: Unit :> a
-     bc = bottomC
-     {-# NOINLINE bc #-}
-
-instance (Applicative (Vec n), IfCat (:>) a) => IfCat (:>) (Vec n a)
- where
-   ifC :: Bool :* (Vec n a :* Vec n a) :> Vec n a
-   ifC = go (pure ())
-    where
-      go :: Vec m () -> Bool :* (Vec m a :* Vec m a) :> Vec m a
-      go ZVec = abstC . unitIf . second (twiceP reprC)
-      go (() :< units) = abstC
-                       . ((ifc . second (twiceP exl)) &&& (go units . second (twiceP exr)))
-                       . second (twiceP reprC)
-      ifc :: Bool :* (a :* a) :> a
-      ifc = ifC
-      {-# NOINLINE ifc #-}
-
--- Without these NOINLINE pragmas, GHC's typechecker does exponential work for
--- n-ary trees.
-
-#if 0
-reprC :: Vec (S m) a :> Rep (Vec (S m) a)
-twiceP reprC :: Vec (S m) a :* Vec (S m) a :> Rep (Vec (S m) a) :* Rep (Vec (S m) a)
-second (twiceP reprC)
-  :: Bool :* (Vec (S m) a :* Vec (S m) a) :> Bool :* (Rep (Vec (S m) a) :* Rep (Vec (S m) a))
-  :: Bool :* (Vec (S m) a :* Vec (S m) a) :> Bool :* (a :* Vec m a) :* (a :* Vec m a)
-
-second (twiceP exl) :: Bool :* (a :* Vec m a) :* (a :* Vec m a) :> Bool :* a :* a
-ifc . second (twiceP exl) :: Bool :* (a :* Vec m a) :* (a :* Vec m a) :> a
-
-second (twiceP exr)
-  :: Bool :* (a :* Vec m a) :* (a :* Vec m a) :> Bool :* (Vec m a :* Vec m a)
-go units :: Bool :* (Vec m a :* Vec m a) :> Vec m a
-go units . second (twiceP exr) :: Bool :* (a :* Vec m a) :* (a :* Vec m a) :> Vec m a
-
-(ifc . second (twiceP exl)) &&& (go units . second (twiceP exr))
-  :: Bool :* (a :* Vec m a) :* (a :* Vec m a) :> a :* Vec m a
-
-abstC . ((ifc . second (twiceP exl)) &&& (go units . second (twiceP exr))) . second (twiceP reprC)
-  :: Bool :* (Vec (S m) a) :* (Vec (S m) a) :> Vec (S m) a
-#endif
-
--- TODO: Look for simple formulations
-
--- Without NOINLINE pragmas, GHC's typechecker does exponential work for
--- trees.
---
--- TODO: Try again without NOINLINE, since I've reworked these definitions.
-
--- TODO: Abstract these definitions into something reusable.
-
-#endif
+-- Needs UndecidableInstances
