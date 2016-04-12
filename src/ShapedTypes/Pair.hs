5c4e657893b0fe2481ca62083a352559480be7ff
Conal Elliott <conal@conal.net>
2016-04-11 19:07:18 -0700

    move circuit support from Pair to Types.Pair

--- src/ShapedTypes/Pair.hs
+++ src/ShapedTypes/Pair.hs
@@ -6,13 +6,6 @@
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeFamilies       #-}
 
--- For circuit support
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TypeOperators         #-}
-
 {-# OPTIONS_GHC -Wall -fno-warn-orphans #-}
 
 -- {-# OPTIONS_GHC -fno-warn-unused-imports #-} -- TEMP
@@ -35,11 +28,9 @@
 -- {-# OPTIONS_GHC -fplugin-opt=ReificationRules.Plugin:trace #-}
 -- {-# OPTIONS_GHC -ddump-rule-rewrites #-}
 
-module ShapedTypes.Pair (Pair(..)) where
+module ShapedTypes.Pair (Pair(..),pswap) where
 
-import Prelude hiding (id,(.))
-import Data.Monoid ({-Monoid(..),-}(<>))
-import Control.Category (id,(.))
+import Data.Monoid ((<>))
 import Control.Applicative (liftA2)
 -- import Data.Typeable (Typeable)
 -- import Data.Data (Data)
@@ -47,14 +38,6 @@ import Control.Applicative (liftA2)
 import Test.QuickCheck (Arbitrary(..),CoArbitrary(..))
 
 import Data.Key
-
--- import Circat.Rep
-
-import Circat.Category (Uncurriable(..),twiceP,(***),(&&&),second,ProductCat(..))
-import Circat.Classes (BottomCat(..),IfCat(..),IfT)
-import Circat.Circuit
-import Circat.Misc ((:*))
-#include "Circat/AbsTy.inc"
 
 import ShapedTypes.ApproxEq
 import ShapedTypes.Sized
@@ -144,46 +127,8 @@ instance LScan Pair where
   {-# INLINE lscan #-}
 
 {--------------------------------------------------------------------
-    Circuit support
+    Miscellany
 --------------------------------------------------------------------}
 
-instance GenBuses q_q => Uncurriable (:>) q_q (Pair a) where
-  uncurries = id
-
-instance GenBuses a => GenBuses (Pair a) where
-  genBuses' prim ins = abstB <$> (PairB <$> gb <*> gb)
-   where
-     gb :: BusesM (Buses a)
-     gb = genBuses' prim ins
-     {-# NOINLINE gb #-}
-  delay (a :# b) = abstC . (del a *** del b) . reprC
-   where
-     del :: a -> (a :> a)
-     del = delay
-     {-# NOINLINE del #-}
-  ty = const (PairT t t)
-   where
-     t = ty (undefined :: a)
-     {-# NOINLINE t #-}
-
--- Without these NOINLINE pragmas, GHC's typechecker does exponential work for
--- binary trees.
-
-instance BottomCat (:>) a => BottomCat (:>) (Pair a) where
-  bottomC = abstC . (bc &&& bc)
-   where
-     bc :: () :> a
-     bc = bottomC
-     {-# NOINLINE bc #-}
-
-instance IfCat (:>) a => IfCat (:>) (Pair a)
- where
-   ifC = abstC . pairIf . second (twiceP reprC)
-
--- Specialization of prodPair
-pairIf :: forall k a. (ProductCat k, IfCat k a) => IfT k (a :* a)
-pairIf = half exl &&& half exr
-  where
-    half :: (u `k` a) -> ((Bool :* (u :* u)) `k` a)
-    half f = ifC . second (twiceP f)
-    {-# NOINLINE half #-}
+pswap :: Pair a -> Pair a
+pswap (a :# b) = b :# a
--- src/ShapedTypes/Types/Pair.hs
+++ src/ShapedTypes/Types/Pair.hs
@@ -1,5 +1,13 @@
+{-# LANGUAGE CPP                #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE DeriveDataTypeable #-}
+-- {-# LANGUAGE DeriveDataTypeable #-}
+
+-- For circuit support
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeOperators         #-}
 
 {-# OPTIONS_GHC -Wall #-}
 
@@ -20,15 +28,26 @@
 
 module ShapedTypes.Types.Pair (Pair(..)) where
 
-import Data.Typeable (Typeable)
-import Data.Data (Data)
+import Prelude hiding (id,(.))
+import Control.Category (id,(.))
+
+-- import Data.Typeable (Typeable)
+-- import Data.Data (Data)
+
+-- import Circat.Rep
+
+import Circat.Category (Uncurriable(..),twiceP,(***),(&&&),second,ProductCat(..))
+import Circat.Classes (BottomCat(..),IfCat(..),IfT)
+import Circat.Circuit
+import Circat.Misc ((:*))
+#include "Circat/AbsTy.inc"
 
 import Circat.Rep (HasRep(..))
 
 infixl 1 :#
 -- | Uniform pairs
 data Pair a = a :# a
-  deriving (Show,Typeable,Data)
+  deriving (Show)  -- ,Typeable,Data
 
 --   deriving (Functor,Traversable,Eq,Show,Typeable,Data,Generic,Generic1)
 
@@ -36,3 +55,48 @@ instance HasRep (Pair a) where
   type Rep (Pair a) = (a,a)
   repr (a :# a') = (a,a')
   abst (a,a') = (a :# a')
+
+{--------------------------------------------------------------------
+    Circuit support
+--------------------------------------------------------------------}
+
+instance GenBuses q_q => Uncurriable (:>) q_q (Pair a) where
+  uncurries = id
+
+instance GenBuses a => GenBuses (Pair a) where
+  genBuses' prim ins = abstB <$> (PairB <$> gb <*> gb)
+   where
+     gb :: BusesM (Buses a)
+     gb = genBuses' prim ins
+     {-# NOINLINE gb #-}
+  delay (a :# b) = abstC . (del a *** del b) . reprC
+   where
+     del :: a -> (a :> a)
+     del = delay
+     {-# NOINLINE del #-}
+  ty = const (PairT t t)
+   where
+     t = ty (undefined :: a)
+     {-# NOINLINE t #-}
+
+-- Without these NOINLINE pragmas, GHC's typechecker does exponential work for
+-- binary trees.
+
+instance BottomCat (:>) a => BottomCat (:>) (Pair a) where
+  bottomC = abstC . (bc &&& bc)
+   where
+     bc :: () :> a
+     bc = bottomC
+     {-# NOINLINE bc #-}
+
+instance IfCat (:>) a => IfCat (:>) (Pair a)
+ where
+   ifC = abstC . pairIf . second (twiceP reprC)
+
+-- Specialization of prodPair
+pairIf :: forall k a. (ProductCat k, IfCat k a) => IfT k (a :* a)
+pairIf = half exl &&& half exr
+  where
+    half :: (u `k` a) -> ((Bool :* (u :* u)) `k` a)
+    half f = ifC . second (twiceP f)
+    {-# NOINLINE half #-}
